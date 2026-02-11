7cb81deb8c197f15749be46d0c901ba9fae40083
arouliaizzati <arouliaizzati@apps.ipb.ac.id>
2026-02-11 18:45:28 +0700

    Latihan 1.py

--- Pertemuan 3/Latihan 1.py
+++ Pertemuan 3/Latihan 1.py
@@ -1,7 +1,3 @@
-
-
-
-
 #======================================
 #Latihan 1: Implementasikan	fungsi untuk menghapus node dengan nilai tertentu.
 #======================================
@@ -58,51 +54,9 @@ ll.delete_node(9)
 ll.display()
 
 
-#======================================
-#Latihan 2: Buat kode implementasikan pencarian pada node tertentu single circular linked list
-#======================================
-
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-        pass
-
-class CircularSinglyLinkedList:
-    def	__init__(self):
-        self.head = None
-        self.tail =	None # Tambahkan pointer tail
-
-    def	insert_at_end(self,	data):
-        new_node = Node(data)
-        if	not	self.head: # Jika linked list kosong
-            self.head	=	new_node
-            self.tail	=	new_node
-            self.tail.next	=	self.head #	Circular link ke dirinya sendiri
-        else:
-            self.tail.next = new_node #	Sambungkan tail	ke node	baru
-            self.tail =	new_node # Update tail ke node baru
-            self.tail.next = self.head # Circular link kembali ke head
-               
-    def display (self):
-        if not self.head:
-            print("list is empty")
-            return
-        print("Circular linked list traversal: ")
-        temp = self.head
-        print(temp.data, end="->")
-        temp = temp.next
-
-        while temp != self.head:
-            print(temp.data, end="->")
-            temp = temp.next
-
-        print("....(back to head)")
-
-
-
 #masukkan datanya
 cll= CircularSinglyLinkedList
 for x in [3,6,34,7,13,5]:
     cll.insert_at_end(x)
 cll.display()
+
